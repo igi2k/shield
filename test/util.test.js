f7aa0a0793144f87a478221e2c00c566dc78f587
IGI2k <pavel.nejedly@atlas.cz>
2018-08-08 01:36:42 +0200

    refactored number padding

--- test/util.test.js
+++ test/util.test.js
@@ -1,8 +1,10 @@
 const assert = require("assert");
 
 describe("Utility", function () {
+
+    const formatTime = require("../lib/util/format-time");
+
     describe("format-time", function () {
-        const formatTime = require("../lib/util/format-time");
 
         it("undefined", function(){
             assert.equal(undefined, formatTime(undefined));
@@ -34,5 +36,22 @@ describe("Utility", function () {
             assert.equal("-5.300s", formatTime(-5300));
             assert.equal("-500ms", formatTime(-500));
         });
+    });
+
+    describe("pad-number", function() {
+        const padNumber = formatTime.padNumber;
+
+        it("shorter", function(){
+            assert.equal("10", padNumber(10, 1));
+        });
+
+        it("equal", function(){
+            assert.equal("10", padNumber(10, 2));
+        });
+
+        it("larger", function(){
+            assert.equal("007", padNumber(7, 3));
+        });
+
     });
 });
