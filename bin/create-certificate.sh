e0227c844b73bdaa5b4b6d53200772be8b08fac6
IGI2k <pavel.nejedly@atlas.cz>
2019-04-14 13:22:42 +0200

    added support for certificate authentication

--- bin/create-certificate.sh
+++ bin/create-certificate.sh
@@ -2,6 +2,7 @@
 # I/O configuration
 ca_name=ca
 server_name=shield
+client_name=client
 
 # fix for loading cerificate config
 SCRIPT_LINK=$(readlink $0)
@@ -57,3 +58,33 @@ authorityKeyIdentifier = keyid,issuer
 subjectAltName         = DNS:${CN}
 EXT
 )
+
+# Generate client certificate
+if [[ $1 != "-client" ]]; then
+  exit
+fi  
+
+# Client
+client_config_file="${CONF_DIR}/${client_name}-csr.conf"
+client_key_file=${client_name}-key.pem
+client_csr_file=${client_name}-csr.pem
+client_cert_file=${client_name}-cert.pem
+client_p12_file=${client_name}.p12
+
+# generate Client CSR
+if [[ ! -f ${client_key_file} ]] || [[ ! -f ${client_csr_file} ]]; then
+  openssl genrsa -out ${client_key_file} 2048
+  openssl req -new -sha256 -config ${client_config_file} -key ${client_key_file} -out ${client_csr_file}
+fi
+
+# sign Client
+openssl x509 -sha256 -req -days 365 -in ${client_csr_file} -CA ${ca_cert_file} -CAkey ${ca_key_file} -CAcreateserial -out ${client_cert_file} -extensions v3_ca -extfile <(cat << EXT
+[ v3_ca ]
+basicConstraints       = CA:false
+subjectKeyIdentifier   = hash
+authorityKeyIdentifier = keyid,issuer
+EXT
+)
+
+# package Client cert + key
+openssl pkcs12 -export -out ${client_p12_file} -inkey ${client_key_file} -in ${client_cert_file} -certfile ${ca_cert_file}
